2c8a2d6d032b08f3189ba464147a954a9a861377
pbuczma <pbuczma@poczta.onet.pl>
2021-11-21 16:43:29 +0100

    Create releasenotes.groovy

---
+++ vars/releasenotes.groovy
@@ -0,0 +1,18 @@
+import java.io.*;
+import groovy.io.*;
+
+def dir = new File( pwd() );
+
+
+def call(Map config=[:]) {
+    new File(dir.path + "/releasenotes.txt").withWriter('utf-8') { writer ->
+
+        dir.eachFileRecurse(FileType.ANY) { file ->
+            if (file.isDirectory()) {
+                writer.writeLine(file.name);
+            } else {
+                writer.writeLine("\t" + file.name + "\t" + file.length());
+            }
+        }
+    }
+}
