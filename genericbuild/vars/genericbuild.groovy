b232556f5a917b8144ebc38b9f2892325db1bab6
Piotr Buczma <pbuczma@poczta.onet.pl>
2021-12-20 15:17:54 +0100

    Initial pipline library

---
+++ genericbuild/vars/genericbuild.groovy
@@ -0,0 +1,54 @@
+def call(Map config[]=[:]) {
+
+    pipeline {
+        agent none
+        stages {
+            stage('SCM') {
+                agent {
+                    docker {
+                        image 'alpine'
+                    }
+                }
+       step s{
+        echo 'Gathering code from version control'
+        git branch : '${branch}', url: 'https://github.com/pbuczma/step3.git'
+
+       }
+      }
+      stage('Build'){
+       agent {
+           docker {
+               image 'mcr.microsoft.com/dotnet/sdk'
+               // label 'maven-label'
+           }
+       }
+       environment {
+          HOME = '/tmp'
+       }
+       step s{
+            //try{
+               echo 'Building...'
+               sh 'cd ConsoleApp1'
+               sh 'dotnet build  ConsoleApp1'
+               echo 'Building New Feature'
+               releasenotes(changes: "true")
+             //}catch(ex){
+             //  echo  'Something went wrong'
+             //  echo  ex.toString();
+             //  currentBuild.result = 'FAILURE'
+             //}
+       }
+      }
+      stage('Test'){
+       step s{
+        echo  'Testing...'
+       }
+      }
+      stage('Deploy'){
+       step s{
+        echo 'Deploying...'
+       }
+      }
+     }
+    }
+}
